5fecd638fb1f6e08fdce73721021a53ad097b83c
socome6586-coder/Seok-dev <socome6586@gmail.com>
2026-03-24 21:42:52 +0900

    HelloJava- else if 문까지 연습

---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperationEx3 {
+
+	public static void main(String[] args) {
+
+		int score = 80;
+		boolean sum = score >= 80 && score <= 100;
+		System.out.println(sum);
+	}
+
+}
