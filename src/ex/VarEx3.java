8c9a0a5916ba33e9f4385032047ac1c9c117cccf
socome6586-coder/Seok-dev <socome6586@gmail.com>
2026-03-24 19:39:17 +0900

    first commit

---
+++ src/ex/VarEx3.java
@@ -0,0 +1,15 @@
+package ex;
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+		long l = 100000000L;
+		boolean b = true;
+		
+		System.out.println(l);
+		System.out.println(b);
+		
+			
+	}
+
+}
---
+++ src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+
+	public static void main(String[] args) {
+		//정수
+		byte b = 127; //-128~127
+		short s = 32767; // -32,768~32,767
+		int i = 2147483647; // -21억~21억
+		long l = 9223372036854775807L;
+		
+		//실수
+		float f = 10.0f; // 오류가 많아 실무에서 거의 사용X
+		double d = 10.0;
+	}
+
+}
