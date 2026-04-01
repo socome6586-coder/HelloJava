f3998bb3b9f23bc93b24058b32586d55dfeaaeb3
socome6586-coder/Seok-dev <socome6586@gmail.com>
2026-04-01 20:56:53 +0900

    scope에 대해서

--- src/loop/While2_3.java
+++ src/loop/While2_3.java
@@ -4,8 +4,8 @@ public class While2_3 {
 
 	public static void main(String[] args) {
 		int sum = 0;
-		int i = 10;
-		int endNum = 100;
+		int i = 1;
+		int endNum = 3;
 
 		//i=4, endNum=3
 		while (i <= endNum) {
--- src/scope/Scope2.java
+++ src/scope/Scope2.java
@@ -1,14 +1,15 @@
 package scope;
 
-public class Scope1 {
+public class Scope2 {
 
 	public static void main(String[] args) {
 		int m = 10;
-		if(true) {
-			int x = 20;
+		for (int i = 0; i < 2; i++) { // 
 			System.out.println(m);
-			System.out.println(x);
-		} // x 생존 종료
+			System.out.println(i);
+		} // i 생존 종료
+		
 		System.out.println(m);
-	} // m 생존 종료
+	}
+
 }
